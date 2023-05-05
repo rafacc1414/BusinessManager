f38f0018f0b12e7a6507644e550f5e64e8053dab
rafacc <rafacc1414@gmail.com>
2023-05-05 21:55:17 +0200

    BusinessManager: Add CRUD interfaces
    A CRUD interface is added for the models. It has been implemented
    making use of templates.
    
    A CRUD interface is added for the views.
    
    Also a new type has been created for list all the CRUD options. It
    contains an extra option which must be deleted in the final
    release.

--- include/GeneralTypes.hpp
+++ include/GeneralTypes.hpp
@@ -1,4 +1,14 @@
 #ifndef GENERAL_TYPES_HPP
 #define GENERAL_TYPES_HPP
 
+typedef enum Crud_options
+{
+    NO_OPTION = 0,
+    SHOW_ALL, // TODO: DELETE in final version
+    CREATE,
+    READ,
+    UPDATE,
+    DELETE
+} crud_options;
+
 #endif
